106ef73fb6fa3bbc125a01c4fc2ccbf7be834b0c
Zachary Wang <zachary.zh.wang@gmail.com>
2026-02-15 18:18:16 -0800

    add introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Zachary Wang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi! I'm Zachary, and I'm a freshman from California. I play soccer competitively and I enjoy watching football and F1 in my free time.")
 if __name__ == "__main__":
     main()
